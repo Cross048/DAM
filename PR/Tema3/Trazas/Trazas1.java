cb2d628515f5ad396b52dffb41aabb5fe2910c5e
Cross048 <115544159+Cross048@users.noreply.github.com>
2022-10-27 00:35:13 +0200

    Add files via upload

---
+++ PR/Tema3/Trazas/Trazas1.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tema3.Trazas;
+
+/**
+ *
+ * @author Cristian Bernal Mendez
+ */
+public class Trazas1 {
+    
+    public static void main(String ar[]){
+        int i,suma=0;
+        i=1;
+        while(i!=10){
+            suma=suma+i;
+            i++;
+        }
+        System.out.println(suma);
+    }
+    
+}
