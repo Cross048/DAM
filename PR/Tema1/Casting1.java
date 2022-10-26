a3cef20b299c757ce685c6b407dc2a91399da4ae
Cross048 <115544159+Cross048@users.noreply.github.com>
2022-10-27 00:27:54 +0200

    Add files via upload

---
+++ PR/Tema1/Casting1.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tema2;
+
+/**
+ *
+ * @author Cristian Bernal Mendez
+ */
+public class Casting1 {
+    public static void main(String[] args) {
+        byte edadJuan = 20;
+        byte edadPedro = edadJuan+1;
+        
+        short sueldoBase = 1980;
+        short complementos = 400;
+        
+        short sueldoTotal;
+        sueldoTotal=(short) (sueldoBase+complementos);
+ }
+} //pag 15 Bboadilla
+
